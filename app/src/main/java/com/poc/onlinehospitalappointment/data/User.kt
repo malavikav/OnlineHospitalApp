320e4e9ffb7a89a38e0d1af4e4a72f514b146a79
Malavika <malavika.android@gmail.com>
2022-11-08 15:42:11 +0530

    Compelted login and signup

--- app/src/main/java/com/poc/onlinehospitalappointment/data/User.kt
+++ app/src/main/java/com/poc/onlinehospitalappointment/data/User.kt
@@ -1,4 +1,8 @@
 package com.poc.onlinehospitalappointment.data
 
-data class User (val fname: String?, val lname: String?, val type: String?,
-                 val email:String?,val password:String?)
+data class User (val uid :String?,val fname: String?, val lname: String?, val type: String?,
+                 val email:String?,val password:String?,val age: String,
+                 val dob: String?,
+                 val gender: String?,
+                 val number: String?,
+                 val  enumber: String?)
