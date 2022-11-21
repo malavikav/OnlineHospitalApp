c90ab08b6e60c1481ea602d8eda9640b77dbb467
pavaac <pavan.ac.theju@gmail.com>
2022-11-21 14:48:30 +0530

    display doctor list in patient appointment

--- app/src/main/java/com/poc/onlinehospitalappointment/adapter/DoctorAdapter.kt
+++ app/src/main/java/com/poc/onlinehospitalappointment/adapter/DoctorAdapter.kt
@@ -8,54 +8,56 @@ import android.widget.TextView
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.poc.onlinehospitalappointment.R
+import com.poc.onlinehospitalappointment.data.Approving
 import com.poc.onlinehospitalappointment.data.Doctor
+import com.poc.onlinehospitalappointment.data.DoctorList
 import com.poc.onlinehospitalappointment.listeners.AppointmentCallback
 
-class DoctorAdapter(
-    private val DoctorList: ArrayList<Doctor>,
-    private val appointmentCallback: AppointmentCallback
-) : RecyclerView.Adapter<DoctorAdapter.MyViewHolder>() {
+class DoctorAdapter : RecyclerView.Adapter<DoctorAdapter.MyViewHolder>() {
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var DImage: ImageView = itemView.findViewById(R.id.dc_1)
-        val DName: TextView = itemView.findViewById(R.id.dc_name)
-        val DSpec: TextView = itemView.findViewById(R.id.dc_spec)
-        val DDes: TextView = itemView.findViewById(R.id.dc_spe)
-        val DDeg: TextView = itemView.findViewById(R.id.dc_deg)
-        val DBtn: AppCompatTextView = itemView.findViewById(R.id.dc_btn)
-    }
+    private val userList = ArrayList<DoctorList>()
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.appointmentd_card_view,
+            parent,false
+        )
+        return MyViewHolder(itemView)
 
-        val cardItem =
-            LayoutInflater.from(parent.context)
-                .inflate(R.layout.appointmentd_card_view, parent, false)
-        return MyViewHolder(cardItem)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val doctor = DoctorList[position]
-        holder.DImage.setImageResource(doctor.DImage)
-        holder.DName.text = doctor.DName
-        holder.DSpec.text = doctor.DSpec
-        holder.DDes.text = doctor.DDes
-        holder.DDeg.text = doctor.DDeg
-//        holder.DBtn.text = doctor.DBtn
 
-        holder.DBtn.setOnClickListener {
-            appointmentCallback.onBookClick(DoctorList!![position])
-            //fun onBookClick(view: View) {
+        val currentitem = userList[position]
 
-//            val intent = Intent(it.context, BookingFragment::class.java)
-//            intent.putExtra("position",holder.adapterPosition)
-//            it.context.startActivity(intent)
-        }
+        holder.fname.text = currentitem.fname
+//        holder.DImage
+//        holder.DSpec.text = currentitem.DSpec
+//        holder.DDes.text = currentitem.DDes
+//        holder.DDeg.text = currentitem.DDeg
+
 
     }
 
     override fun getItemCount(): Int {
-        return DoctorList.size
+        return userList.size
     }
 
+    fun updateUserList(userList : List<DoctorList>){
+
+        this.userList.clear()
+        this.userList.addAll(userList)
+        notifyDataSetChanged()
+
+    }
+
+    class  MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        val fname : TextView = itemView.findViewById(R.id.dc_name)
+//        val DSpec : TextView = itemView.findViewById(R.id.dc_spec)
+//        val DDes : TextView = itemView.findViewById(R.id.dc_spe)
+//        val DDeg : TextView = itemView.findViewById(R.id.dc_deg)
+    }
 
 }
